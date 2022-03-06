28406e1bb3b0e9679012548850dd188c31382cad
garimayadav97 <garimaydv777@gmail.com>
2022-03-06 12:41:02 +0530

    Add sample code

---
+++ src/main/java/com/lucus/assignment/samplespringbootproject/SampleApplication.java
@@ -0,0 +1,13 @@
+package com.lucus.assignment.samplespringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
